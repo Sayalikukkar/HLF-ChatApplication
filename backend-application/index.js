3475392db15ba753bce428e9f6897d8435a28888
Sayalikukkar <sayukukkar11@gmail.com>
2023-09-19 20:07:26 +0530

    enroll register file

--- backend-application/index.js
+++ backend-application/index.js
@@ -1,15 +1,19 @@
 const express = require("express");
 const cors = require("cors");
-// const dotenv = require("dotenv");
+const dotenv = require("dotenv");
 
 const app = express();
 dotenv.config();
 
-// app.use(cors());
+app.use(cors());
 app.use(cors({ origin: true }));
 app.use(express.json());
 
-// const PORT = process.env.PORT || 1000;
+app.get("/", (req,res) => {
+  res.send("hello, welcome to my page");
+});
+
+const PORT = process.env.PORT || 1000;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
